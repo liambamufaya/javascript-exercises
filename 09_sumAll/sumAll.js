3e630bb5e36e20896e9f9699bbb77c8c4545fa21
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2025-12-22 17:36:02 +0200

    Add to the cloud

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+   if ((num1 < 0 || !Number.isInteger(num1)) || (num2 < 0 || !Number.isInteger(num2)) || (num1 ==NaN || num2 ==NaN )){
+        return "ERROR"
+   }
 
+   let sum = 0;
+   if (num1 > num2){
+        for(let i=num2; i <= num1; i++){
+ 
+    sum+=i
+}
+    return sum;
+
+   }
+for(let i=num1; i <= num2; i++){
+ 
+    sum+=i
+}
+    return sum;
 };
 
 // Do not edit below this line
