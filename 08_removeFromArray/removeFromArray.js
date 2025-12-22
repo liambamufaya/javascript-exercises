712fb3ea853b5655dae1eb0743b94ac7f1407b2d
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2025-12-22 09:36:45 +0200

    Stuffs

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(theObject, ...number) {
+    const newarray = theObject.filter((item) => !number.includes(item));
+    return newarray
+
 };
 
 // Do not edit below this line
